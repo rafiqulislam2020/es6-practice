5aac684d368f9a31bedb28fc3e22ba3d4ccf80be
Md.Rafiqul Islam <shopon104@gmail.com>
2020-03-05 00:01:24 +0600

    first commit

---
+++ class.js
@@ -0,0 +1,11 @@
+class Student{
+    constructor(){
+        this.id = 1;
+        this.name = "Rahim";
+    }
+
+}
+
+const student1 = new Student();
+const student2 = new Student();
+console.log(student1,student2);
---
+++ inheritance.js
@@ -0,0 +1,10 @@
+class Child{
+    constructor(name){
+        this.name = name;
+
+    }
+}
+
+const baby = new  Child ("kakon");
+const baby2 = new Child("jo");
+console.log(baby);
